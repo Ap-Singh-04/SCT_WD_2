6240145764a8247a673ba972ebbbb6561ad5501a
Ap-Singh-04 <154320409+Ap-Singh-04@users.noreply.github.com>
2025-08-20 20:23:13 +0530

    Update ApCalc.js

--- ApCalc.js
+++ ApCalc.js
@@ -6,7 +6,7 @@ const radBtn = document.getElementById('rad');
 const modeSwitchBtn = document.getElementById('modeSwitch');
 
 let expr = '', mode = 'DEG', memory = 0;
-let calcType = 'Standard'; // or 'scientific'
+let calcType = 'scientific'; // or 'standard'
 
 // Button layouts
 const simpleButtons = [
@@ -104,7 +104,7 @@ modeSwitchBtn.onclick = () => {
 };
 
 // Initial load
-loadKeys(simpleButtons);
+loadKeys(scientficButtons);
 
 
 
